8d356743e01e9995e3620222679fb90d478c1d7a
unknown <ashishnehete6@gmail.com>
2023-12-05 12:20:18 +0530

    new com

---
+++ Ex4.java
@@ -0,0 +1,14 @@
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+          Byte b=10;
+          Byte bObj1 = b;
+          Byte bObj2 = new Byte(b);
+          System.out.println("b ="+b);
+          System.out.println(" bObj1="+bObj1);
+          System.out.println(" bObj2="+bObj2);
+
+	}
+
+}
